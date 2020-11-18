53d991c0ac1916902a65b34afd7f93b9e3d5f868
Jan Blomerus <blomerusjm@ufs.ac.za>
2020-11-18 17:26:34 +0200

    2

--- src/LifeValFunc/src/LifeValFunc.jl
+++ src/LifeValFunc/src/LifeValFunc.jl
@@ -1,5 +1,47 @@
 module LifeValFunc
 
-greet() = print("Hello World!")
+using JuliaDB, DataFrames
 
-end # module
+using LifeValStructures
+
+export Table_to_Policies, Table_Length
+
+function unitPFunction(initVal, incr, rows)
+    unitP = [initVal] .* (1 .+ incr) .^ (1 / 12) #unitP[1] is at end of first month
+    for i in 2:rows
+        append!(unitP, unitP[i-1] .* (1 .+ incr) .^ (1 / 12))
+    end
+    return(unitP)
+end
+
+function Table_to_Policies(policyTable::IndexedTable, productIndex, columnsIndex, PolicyType)
+    #global PolicyType
+    n = length(policyTable)
+    # convert to policy and append to vector
+    # change column order to correspond to fields                                                         of Policy
+    orderedInput = JuliaDB.select(policyTable, columnsIndex)
+    # create product identifier in vector
+    product = JuliaDB.select(policyTable, productIndex)
+    PolicyVector = Vector{LifeValStructures.Policy}(undef, n)
+    # eventually create struct called policyVector
+    for i in 1:n
+        type = PolicyType[product[i]]
+        PolicyVector[i] = type(orderedInput[i]...)
+    end
+
+    return PolicyVector
+end
+
+function Table_Length(policyTable::Array{IndexedTable}, runsettings)
+    n = length(policyTable)
+    maxlength = 0
+    for i in 1:n
+        if length(policyTable[i]) > maxlength
+            maxlength = length(policyTable[i])
+        end
+    end
+    reserveList = Array{Float64}(undef, maxlength, nrow(runsettings))
+    return reserveList
+end
+
+end
